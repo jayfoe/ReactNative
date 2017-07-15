18cf9a985990d268dc332ace83e444fea2991906
jonathan <jonford3@hotmail.com>
2017-07-15 03:33:19 -0700

    hello world printed on screen

---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => (
+  <Text>Hello World</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
